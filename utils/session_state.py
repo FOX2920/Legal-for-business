25d3f9668f9b26321392fe184971d3a21109d8de
TRAN THANH SON <91709267+FOX2920@users.noreply.github.com>
2025-03-22 10:40:56 +0700

    Update session_state.py

--- utils/session_state.py
+++ utils/session_state.py
@@ -3,26 +3,26 @@ from utils.prepare_vectordb import get_vectorstore
 
 def initialize_session_state_variables(st):
     """
-    Initialize session state variables for the Streamlit application
+    Khởi tạo các biến trạng thái phiên cho ứng dụng Streamlit
 
-    Parameters:
-    - st (streamlit.delta_generator.DeltaGenerator): Streamlit's DeltaGenerator object used for rendering elements
+    Tham số:
+    - st (streamlit.delta_generator.DeltaGenerator): Đối tượng DeltaGenerator của Streamlit được sử dụng để hiển thị các phần tử
     """
-    # Get the list of uploaded documents
+    # Lấy danh sách tài liệu đã tải lên
     upload_docs = os.listdir("docs")
-    # List of session state variables to initialize
+    # Danh sách các biến trạng thái phiên để khởi tạo
     variables_to_initialize = ["chat_history", "uploaded_pdfs", "processed_documents", "vectordb", "previous_upload_docs_length"]
-    # Iterate over the variables and initializes them if not present in the session state 
+    # Lặp qua các biến và khởi tạo chúng nếu không có trong trạng thái phiên
     for variable in variables_to_initialize:
         if variable not in st.session_state:
             if variable == "processed_documents":
-                # Set to the name of the files present in the docs folder
+                # Đặt thành tên của các tệp có trong thư mục docs
                 st.session_state.processed_documents = upload_docs
             elif variable == "vectordb":
-                # Is set to none if its the first time the app is initialized. If not, is set to the vector database that already exists
+                # Đặt thành None nếu đây là lần đầu tiên ứng dụng được khởi tạo. Nếu không, đặt thành cơ sở dữ liệu vector đã tồn tại
                 st.session_state.vectordb = get_vectorstore(upload_docs, from_session_state=True)
             elif variable == "previous_upload_docs_length":
-                # Set to the quantity of documents in the docs folder during app startup
+                # Đặt thành số lượng tài liệu trong thư mục docs khi khởi động ứng dụng
                 st.session_state.previous_upload_docs_length = len(upload_docs)
             else:
                 st.session_state[variable] = []
