00d00de4e71b9ec74e352c84729fb7a3c8288c82
TRAN THANH SON <91709267+FOX2920@users.noreply.github.com>
2025-03-22 12:21:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,6 @@ from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
-from dotenv import load_dotenv
-
-# Tải biến môi trường từ file .env
-load_dotenv()
 
 # Cấu hình API key
 api_key = os.getenv("GOOGLE_API_KEY")
